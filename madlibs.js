1c0c731888e3454440f8eb06530e844db0a48114
Dilara Katuk <dilarakatuk@gmail.com>
2022-05-16 22:43:29 +0300

    html and css part

--- madlibs.js
+++ madlibs.js
@@ -26,10 +26,10 @@
  * There are multiple ways to do this, but you may want to use regular expressions.
  * Please go through this lesson: https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/regular-expressions/
  */
-function parseStory(rawStory) {
-  // Your code here.
-  return {}; // This line is currently wrong :)
-}
+// function parseStory(rawStory) {
+//   // Your code here.
+//   return {}; // This line is currently wrong :)
+// }
 
 /**
  * All your other JavaScript code goes here, inside the function. Don't worry about
@@ -37,6 +37,61 @@ function parseStory(rawStory) {
  * 
  * You'll want to use the results of parseStory() to display the story on the page.
  */
-getRawStory().then(parseStory).then((processedStory) => {
-  console.log(processedStory);
-});
+// getRawStory().then(parseStory).then((processedStory) => {
+//   console.log(processedStory);
+// });
+
+
+
+
+
+
+// FUNCTIONS FOR BUTTONS AND ANIMATIONS
+
+
+const clickBtn1 = document.querySelector('#BTN');
+const clickBtn2 = document.querySelector('#start');
+const disclaimer = document.querySelector('.disclaimer')
+const topPart= document.querySelector('.disTop');
+const middle = document.querySelector('.disMid');
+const story1 = document.querySelector('.madLibs');
+middle.style.display ="none";
+
+
+
+clickBtn1.addEventListener('click', () => {
+  clickBtn1.style.display ="none"
+  if (middle.style.display == "none"){
+    middle.style.display = "block"
+    topPart.style.display = "none"
+  } else {
+          middle.style.display = "none"
+        }
+  })
+
+
+clickBtn2.addEventListener('click', () =>{
+  clickBtn2.style.display = "none"
+  
+  if(story1.style.display = "none") {
+    story1.style.display ="block"
+    disclaimer.style.display ="none"
+  } 
+    
+})
+
+const inputFilled= document.getElementsByTagName("input")
+
+inputFilled.addEventListener('input', () => {
+  inputFilled.style.backgroundColor ="gray"
+})
+
+
+
+
+
+
+
+
+
+
